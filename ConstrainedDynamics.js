5148081af1eff8aa10f4c5847cc8bfd44a81c21f
Emil Keshishian <42214323+EmilKD@users.noreply.github.com>
2024-08-26 01:03:32 +0330

    Constrained Dynamics Done

---
+++ ConstrainedDynamics.js
@@ -0,0 +1,260 @@
+var canvas = /** @type {HTMLCanvasElement} */ (document.getElementById("myCanvas"));
+var c = canvas.getContext('2d');
+
+canvas.width = window.innerWidth - 20;
+canvas.height = window.innerHeight - 100;
+var simMinWidth = 2.0;
+var cScale = Math.min(canvas.width, canvas.height) / simMinWidth;
+var simWidth = canvas.width / cScale;
+var simHeight = canvas.height / cScale;
+
+function cX(pos){
+    return pos.x * cScale;
+}
+
+function cY(pos){
+    return canvas.height - pos.y * cScale;
+}
+
+class vector2d {
+    constructor(a,b){
+        this.x = a;
+        this.y = b;
+    }
+    
+    add(a){
+        let x = this.x + a.x;
+        let y = this.y + a.y;
+        return new vector2d(x,y);
+    }
+
+    sub(a){
+        let x = this.x - a.x;
+        let y = this.y - a.y;
+        return new vector2d(x,y);
+    }
+
+    scale(a){
+        return new vector2d(this.x*a, this.y*a);
+    }
+
+    mag(){
+        return Math.sqrt(this.x*this.x + this.y*this.y);
+    }
+
+    dot(a){
+        return this.x*a.x + this.y*a.y;
+    }
+
+    perp(){
+        return new vector2d(-this.y, this.x);
+    }
+
+    dir(){
+        return new vector2d(this.scale(1.0/this.mag()).x, this.scale(1.0/this.mag()).y);
+    }
+
+    clone(){
+        return new vector2d(this.x, this.y);
+    }
+}
+
+function ClosestPoint(p, a, b){
+    let ab = b.sub(a);
+    let t = ab.dot(ab);
+    if (t==0){
+        return a.clone();
+    } 
+    t = Math.max(0, Math.min(1, ab.dir().scale(ab.dot(p.sub(a)))));
+    return new vector2d(ab.add(ab.dir.scale(t)));
+}
+
+class circularConstrain{
+    constructor(rad, pos){
+        this.rad = rad;
+        this.pos = pos;
+        this.balls = [];
+    }
+
+    addBall(ball){
+        this.balls.push(ball);
+    }
+
+    getDelta(i = null){ // i is the ball number
+        if (i == null)
+        {
+            let deltas = [];
+            for (let ball of this.balls){
+                delta.push(ball.pos.sub(this.pos).dir().scale(ball.pos.sub(pos).mag()-this.rad))
+            }
+            return deltas;
+        }
+        else
+        {
+            return this.balls[i].pos.sub(this.pos).dir().scale(this.balls[i].pos.sub(this.pos).mag()-this.rad);
+        }
+    }
+
+    simulate(dt){
+        for (let i=0; i<this.balls.length; i++){
+            if (this.balls[i].pos.sub(this.pos)==0)
+                return;
+            else{
+                this.balls[i].pos = this.balls[i].pos.sub(this.getDelta(i));
+                this.balls[i].vel = this.balls[i].pos.sub(this.balls[i].previousPos).scale(1.0/dt);
+            }
+            
+        }
+    }
+}
+
+class Ball {
+    constructor(rad, mass, pos = new vector2d(0,0), vel = new vector2d(0,0), isConstrained = false)
+    {
+        this.rad = rad;
+        this.mass = mass;
+        this.pos = pos;
+        this.previousPos = pos.clone();
+        this.vel = vel;
+        this.isConstrained = isConstrained;
+    }
+
+    simulate(dt, a){
+            this.vel.y += a*dt
+            this.previousPos = this.pos.clone();
+            this.pos.x += this.vel.x*dt; 
+            this.pos.y += this.vel.y*dt;
+    }
+}
+
+
+var PhysicsWorld = {
+    dt: 1.0/60.0,
+    substeps: 1,
+    sdt: this.dt/this.substeps,
+    WorldSize: [simWidth, simHeight],
+    g: -9.83,
+    e: 1.0, //coefficient of restitution
+    balls: [],
+    constraints: []
+}
+
+
+function WorldSetup(){
+    PhysicsWorld.sdt = PhysicsWorld.dt / PhysicsWorld.substeps;
+
+    var bead = new Ball(0.05, 1.0, new vector2d(PhysicsWorld.WorldSize[0]/2.0 + 0.2, PhysicsWorld.WorldSize[1]/2.0 + 0.2), new vector2d(0.0, 0.0), true);
+    PhysicsWorld.balls.push(bead);
+    var bead2 = new Ball(0.07, 1.0, new vector2d(PhysicsWorld.WorldSize[0]/2.0 + 0.1, PhysicsWorld.WorldSize[1]/2.0 - 0.2), new vector2d(0.0, 0.0), true);
+    PhysicsWorld.balls.push(bead2);
+    var bead3 = new Ball(0.04, 1.0, new vector2d(PhysicsWorld.WorldSize[0]/2.0 + 0.2, PhysicsWorld.WorldSize[1]/2.0 - 0.2), new vector2d(0.0, 0.0), true);
+    PhysicsWorld.balls.push(bead3);
+    var bead4 = new Ball(0.06, 1.0, new vector2d(PhysicsWorld.WorldSize[0]/2.0 - 0.1, PhysicsWorld.WorldSize[1]/2.0 - 0.3), new vector2d(0.0, 0.0), true);
+    PhysicsWorld.balls.push(bead4);
+    var bead5 = new Ball(0.03, 1.0, new vector2d(PhysicsWorld.WorldSize[0]/2.0 - 0.15, PhysicsWorld.WorldSize[1]/2.0 + 0.0), new vector2d(0.0, 0.0), true);
+    PhysicsWorld.balls.push(bead5);
+    
+    var wire = new circularConstrain(0.6, new vector2d(PhysicsWorld.WorldSize[0]/2.0, PhysicsWorld.WorldSize[1]/2.0));
+    wire.addBall(bead);
+    wire.addBall(bead2);
+    wire.addBall(bead3);
+    wire.addBall(bead4);
+    wire.addBall(bead5);
+    PhysicsWorld.constraints.push(wire);
+
+}
+
+function HandleBallCollision(ball1, ball2){
+    let r = ball2.pos.sub(ball1.pos);
+
+    if (r.mag() == 0 || r.mag() > (ball1.rad + ball2.rad)){
+        return;
+    }
+
+    if (r.mag() < (ball1.rad + ball2.rad)){
+        let normal = r.scale(1.0/r.mag());
+        let v1n = ball1.vel.dot(normal);
+        let v2n = ball2.vel.dot(normal);
+
+        corr = (ball1.rad + ball2.rad - r.mag())/2;
+        ball1.pos = ball1.pos.add(normal.scale(-corr));
+        ball2.pos = ball2.pos.add(normal.scale(corr));
+
+        newv1 = (v1n*ball1.mass + v2n * ball2.mass - ball2.mass * (v1n - v2n) * PhysicsWorld.e) / (ball1.mass + ball2.mass);
+        newv2 = (v1n*ball1.mass + v2n * ball2.mass - ball1.mass * (v2n - v1n) * PhysicsWorld.e) / (ball1.mass + ball2.mass);
+
+        ball1.vel = ball1.vel.add(normal.scale(newv1 - v1n));
+        ball2.vel = ball2.vel.add(normal.scale(newv2 - v2n));
+    }    
+}   
+
+function HandleWallCollision(ball){
+    let rad = ball.rad;
+    let pos = ball.pos;
+    let vel = ball.vel;
+
+    if (pos.x < rad){
+        ball.pos.x = 2*rad - rad;
+        ball.vel.x = - vel.x*PhysicsWorld.e; 
+    } else if (pos.x > simWidth - rad){
+        ball.pos.x = 2*(simWidth - rad) - pos.x;
+        ball.vel.x = - vel.x*PhysicsWorld.e; 
+    }
+    if (pos.y < rad){
+        ball.pos.y = 2*rad - pos.y;
+        ball.vel.y = - vel.y*PhysicsWorld.e; 
+    } else if (pos.y > simHeight - rad){
+        ball.pos.y = 2*(simHeight - rad) - pos.y;
+        ball.vel.y = - vel.y*PhysicsWorld.e; 
+    }
+}
+
+// drawing --------------
+function draw(){
+    c.clearRect(0,0,canvas.width, canvas.height);
+    c.fillStyle = "#FF0000";
+
+    for (const ball of PhysicsWorld.balls){
+        c.beginPath();
+        c.arc(cX(ball.pos), cY(ball.pos), ball.rad*cScale, 0, 2*Math.PI);
+        c.fillStyle = 'rgb(255 0 0)';
+        c.fill();
+    }
+    for(const constraint of PhysicsWorld.constraints){
+        c.beginPath();
+        c.arc(cX(constraint.pos), cY(constraint.pos), constraint.rad*cScale, 0, 2*Math.PI);
+        c.stroke();
+    }
+}
+
+// Simulate --------------
+function simulate(){
+    for (let n=0; n<PhysicsWorld.substeps; n++){
+        for (var i=0; i<PhysicsWorld.balls.length; i++){
+            let ball1 = PhysicsWorld.balls[i];
+            ball1.simulate(PhysicsWorld.sdt, PhysicsWorld.g);
+            for(var j=i+1; j<PhysicsWorld.balls.length; j++){
+                let ball2 = PhysicsWorld.balls[j];
+                HandleBallCollision(ball1, ball2);
+            }
+            for(let constraint of PhysicsWorld.constraints){
+                constraint.simulate(PhysicsWorld.sdt)
+            }
+            
+            HandleWallCollision(ball1);
+        }
+    }  
+}
+
+// Updating --------------
+function update(){
+    simulate();
+    draw();
+    requestAnimationFrame(update);
+}
+
+WorldSetup();
+update();
+// document.getElementById('restitutionSlider').oninput = function(){
+//     PhysicsWorld.e = this.value / 10;
+// }
